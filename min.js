d354c273498668d3eb43258a5bb50aa9a19cdb09
marinacor1 <marinaspam1234@gmail.com>
2016-06-22 13:16:37 -0500

    Add notes to understand

--- min.js
+++ min.js
@@ -3,16 +3,24 @@ var pry = require('pryjs')
 var _= require("lodash");
 
 var hotTowns = function(towns){
+  // pass in towns with temperatures
   var result = { hot: [], warm: []};
+  // this will be outputted at the end
 
   function tempChecker(temp) {
     return temp > 19;
   }
+  // method that returns temps greater than 19
 
    _.forEach(towns, function(hotTemps, town){
+    //  equivalent to enumerable. we iterate through each town
     if (_.every(hotTemps, tempChecker)) {
+      // this is an every function
+      // if every town returns true for tempChecker
       result.hot.push(town);
+      // add the town to the array for hot in result
     } else if (_.some(hotTemps, tempChecker)) {
+      // else if SOME towns are true, push town to warm array
       result.warm.push(town);
     }
   });
@@ -20,3 +28,8 @@ var hotTowns = function(towns){
 };
 
 module.exports = hotTowns;
+
+
+
+
+// I need to make a function that returns a hash of hot and warm towns
